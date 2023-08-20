4e1eca08b853933a620b58ca8791b41e01af1591
unknown <d47797531@alumnos.itr.edu.ar>
2023-08-20 17:57:48 -0300

    css a medias del home

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,10 +1,17 @@
 import React from "react";
+import './Home.css';
+
 
 function Home() {
     return (
-      <div>
-        <h1>home</h1>
+        <>
+      <div className="div">
+        <h1 className="h1">?Paneles Solares¿</h1>
+        <h3 className="h3">Hola, que tal sabes lo que es un panel solar? NO?, nosotros te lo explicamos</h3>
+        <h5 className="h5">Un panel solar es un dispositivo, simple, eficaz y dinamico, que nos permite aprovechar la energia de nuestro sol.
+        La energia que proporciona el sol al planeta es inmensa, ya que una hora de luz solar le permitiria al mundo abastecerce de electricidad por un año aproximadamente</h5>
       </div>
+      </>
     );
   }
   
