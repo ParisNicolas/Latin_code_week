b37df8273f593c8d079a4f86d925195e01fe602b
unknown <d47797531@alumnos.itr.edu.ar>
2023-08-20 17:10:09 -0300

    css a la barra

--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
+import './Navbar.css';
 
 function Navbar() {
     return (
       <>
-      <nav >
-        <ul>
-          <li><Link to="/" >Home</Link></li>
-          <li><Link to="/info">Info</Link></li>
-          <li><Link to="/juego">Juego</Link></li>
-        </ul>
-      </nav>
-  
-     
-  
-      <Outlet></Outlet>
-      </>
+        <nav className="navbar">
+            <ul className="nav-links">
+            <div className="logo">
+                <img src="https://1000marcas.net/wp-content/uploads/2019/12/logo-Renault.png" alt="Logo" />
+            </div>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/info">Info</Link></li>
+                <li><Link to="/juego">Juego</Link></li>
+            <div className="logo">
+                <img src="https://1000marcas.net/wp-content/uploads/2019/12/logo-Renault.png" alt="Logo" />
+            </div>
+            </ul>
+        </nav>
+    <Outlet></Outlet>
+    </>
     );
   }
   
