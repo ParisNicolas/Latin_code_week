2024655d6bca03fe7445854c351fb757437fc4a6
unknown <d47797531@alumnos.itr.edu.ar>
2023-08-21 17:06:11 -0300

    ventajas y paleta de color nueva

--- my-app/src/B-Ven.js
+++ my-app/src/B-Ven.js
@@ -1,13 +1,22 @@
 import React from "react";
+import './B-Ven.css'; 
 
 function Bven(){
 
 return(
     <>
+    <div className="fondo">
+    <div className="tarjeta">
     <h1>Ventajas de los Paneles solares</h1>
     <ul>
-        <h3>Energia renobable y limpia:<div></div></h3>
+        <h4>Energia renobable y limpia:Los paneles solares utilizan la energía del sol para generar electricidad, lo que significa que es una fuente de energía renovable y limpia. No emiten gases de efecto invernadero ni contaminantes atmosféricos durante su operación, lo que contribuye a la reducción de la huella de carbono.</h4>
+        <h4>Reducción de Costos a Largo Plazo: Aunque la inversión inicial en paneles solares puede ser significativa, a largo plazo permiten ahorrar en costos de electricidad. Una vez instalados, los paneles solares pueden generar energía durante varias décadas</h4>
+        <h4>Bajos Costos de Mantenimiento: Los paneles solares requieren muy poco mantenimiento. Solo es necesario limpiarlos periódicamente para asegurar un rendimiento óptimo. Además, la mayoría de los sistemas vienen con garantías de larga duración.</h4>
+        <h4>Aumento del Valor de la Propiedad: La instalación de paneles solares puede aumentar el valor de una propiedad. Los compradores valoran la eficiencia energética y la capacidad de generar su propia electricidad, lo que puede hacer que una propiedad sea más atractiva en el mercado.</h4>
+        <h4>Incentivos y Subsidios: En muchos lugares, hay incentivos gubernamentales y programas de subsidios disponibles para alentar la instalación de sistemas solares. Estos programas pueden reducir significativamente los costos iniciales de inversión.</h4>
     </ul>
+    </div>
+    </div>
     </>
 );
 }
